dbde05819b7a32a6ef5fa30c4acf13c82e2dafac
tomotero1984 <tomotero1984@gmail.com>
2025-05-03 15:53:27 -0400

    Resume - Updating vue-server container to host an ssh server and adding docker user

---
+++ Resume/app/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Welcome>
+      <template #default>
+        <div>
+          <h1>Tom Otero</h1>
+          <h6>Software Engineer</h6>
+        </div>
+      </template>
+    </Welcome>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld,
+  },
+})
+export default class HomeView extends Vue {}
+</script>
