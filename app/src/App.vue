3ed2eb36801ec2c0fdcc145b0189282617a08973
tomotero1984 <tomotero1984@gmail.com>
2025-05-28 14:26:38 -0400

    feature: add ContactView and ProjectsView. Update formatting, style, and functionality. Adding placeholder content (there are errors that need to be fixed)

--- app/src/App.vue
+++ app/src/App.vue
@@ -8,30 +8,8 @@ import Footer from '@/components/Footer.vue'
     <router-view/>
     <Footer/>
   </div>
+  </div>
 </template>
 <style scoped>
 /* Optional styles */
 </style>
-=======
-    <Header />
-    <HomeView />
-    <Footer />
-  </div>
-</template>
-<script lang="ts">
-import { defineComponent } from 'vue'
-import Header from '@/components/Header.vue'
-import HomeView from '@/views/HomeView.vue'
-import Footer from '@/components/Footer.vue'
-
-export default defineComponent( {
-  name: 'App',
-  components: {
-    Header,
-    HomeView,
-    Footer
-  }
-} )
-
-</script>
-
