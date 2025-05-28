d892d281a1f292faa3393cc8907f888db6789454
tomotero1984 <tomotero1984@gmail.com>
2025-05-27 20:03:40 -0400

    feature: create Header and Footer components, remove unused component and css, update gitignore, add scss module to package.json

--- app/src/App.vue
+++ app/src/App.vue
@@ -12,3 +12,26 @@ import Footer from '@/components/Footer.vue'
 <style scoped>
 /* Optional styles */
 </style>
+=======
+    <Header />
+    <HomeView />
+    <Footer />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from '@/components/Header.vue'
+import HomeView from '@/views/HomeView.vue'
+import Footer from '@/components/Footer.vue'
+
+export default defineComponent( {
+  name: 'App',
+  components: {
+    Header,
+    HomeView,
+    Footer
+  }
+} )
+
+</script>
+
