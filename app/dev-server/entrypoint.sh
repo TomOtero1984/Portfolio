25d731b516ba1f80190ed1081083c00f99ac00d0
tomotero1984 <tomotero1984@gmail.com>
2025-06-29 21:07:30 -0400

    task: update docker configs for eventual DockerPreprocessor feature update

---
+++ app/dev-server/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# SSH
+service ssh start
+
+# Website
+serve -l 3333 /app/dist
--- app/vue-server/entrypoint.sh
+++ app/vue-server/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# SSH
-service ssh start
-
 # Website
 serve -l 3333 /app/dist
-
-# tail -f /dev/null
